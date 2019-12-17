d5350f81339d1f5764e92b9003bd83fc2eea4275
EpicTroll1365 <acciowand200@hotmail.com>
2019-12-17 10:02:40 +0200

    Day 1 complete yeet

---
+++ Day1Pt2.py
@@ -0,0 +1,16 @@
+allMasses = []
+
+def calculateFuel() :
+    totalFuel = 0
+    fd = open('./Day1Input.txt','r')
+    for data in fd :
+        allMasses.append(int(data.strip()))
+    for mass in allMasses :
+        fuelToAdd = (mass//3) - 2
+        totalFuel += fuelToAdd
+        while (((fuelToAdd//3) - 2) > 0) :
+            fuelToAdd = (fuelToAdd//3) - 2
+            totalFuel += fuelToAdd
+    print(totalFuel)
+
+calculateFuel()
