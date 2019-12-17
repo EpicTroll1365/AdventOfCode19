86a571227db9c83c0b5aaceb47a6cb84355796e5
EpicTroll1365 <acciowand200@hotmail.com>
2019-12-17 09:52:33 +0200

    First problem yeet

---
+++ Day1.py
@@ -0,0 +1,12 @@
+allMasses = []
+
+def calculateFuel() :
+    totalFuel = 0
+    fd = open('./Day1Input.txt','r')
+    for data in fd :
+        allMasses.append(int(data.strip()))
+    for mass in allMasses :
+        totalFuel += (mass//3) - 2
+    print(totalFuel)
+
+calculateFuel()
