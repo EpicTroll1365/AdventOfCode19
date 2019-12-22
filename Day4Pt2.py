fe8c1a3c7a5d0893bbf65fd96cdb7d23b8d8844f
EpicTroll1365 <acciowand200@hotmail.com>
2019-12-22 11:41:04 +0200

    Day 4 yeet

---
+++ Day4Pt2.py
@@ -0,0 +1,34 @@
+inputYeet = '357253-892942'
+passRange = [int (i) for i in inputYeet.split('-')]
+
+def adjacentNums(num):
+    found = False
+    for i in range(1,len(num)):
+        if num[i-1] == num[i] and num.count(num[i]) == 2:
+            found = True
+            break
+    return found
+
+def findDecrease(num):
+    decrease = False
+    for i in range(1,len(num)):
+        if int(num[i]) < int(num[i-1]):
+            decrease = True
+            break
+    return decrease
+
+
+def findPasswords(passRange):
+    passwords = []
+    for passw in range(passRange[0]+1,passRange[1]): #Within the range bleh
+        strNum = str(passw)
+        if len(strNum) != 6:
+            continue
+        elif not adjacentNums(strNum):
+            continue
+        elif findDecrease(strNum):
+            continue
+        passwords.append(passw)
+    print(len(passwords))
+
+findPasswords(passRange)
